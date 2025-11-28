1f607e18026b6feef382face411f9cf7fc1e9506
sagarboyal <sboyal12@gmail.com>
2025-11-28 18:44:08 +0530

    create app using spring boot

---
+++ src/main/java/com/quickstore/main/QuickStoreApplication.java
@@ -0,0 +1,13 @@
+package com.quickstore.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickStoreApplication.class, args);
+    }
+
+}
